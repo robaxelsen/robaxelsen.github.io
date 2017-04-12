0d4b35039511b33c69c3c57b9ca207d4c8f99fc4
robeerob <rob@rob.ee>
2017-04-12 20:30:19 +0200

    Complete ce-button function, and introduce use of window namespace to avoid problems

--- jscalc/js/index.js
+++ jscalc/js/index.js
@@ -1,43 +1,37 @@
-var numButtons = document.getElementsByClassName('num-button');
-var operatorButtons = document.getElementsByClassName('operator-button');
-var acButton = document.getElementById('ac-button');
-var ceButton = document.getElementById('ce-button');
-var inputField = document.getElementById('input-field');
+window.numButtons = document.getElementsByClassName('num-button');
+window.operatorButtons = document.getElementsByClassName('operator-button');
+window.acButton = document.getElementById('ac-button');
+window.ceButton = document.getElementById('ce-button');
+window.inputField = document.getElementById('input-field');
 
 // Adding click listener to all number buttons, which grabs the value of the clicked button, and adds it to the input field.
 for (var i = 0; i < numButtons.length; i++) {
   numButtons[i].addEventListener('click', function(){
-    inputField.value += this.value;
+    window.inputField.value += this.value;
   }, false);
 };
 
 // Adding click listeners to all operator buttons (except equal sign) and period sign. 
 for (var i = 0; i < operatorButtons.length; i++) {
   operatorButtons[i].addEventListener('click', function(){
-    inputField.value += this.value;
+    var lastChar = window.inputField.value[window.inputField.value.length - 1];
+    if (!lastChar.match(/x|-|\+/)) {  
+      window.inputField.value += this.value;
+    };
   }, false);
 };
 
 // Adding 'ac-button' click listener which resets whole input field
 acButton.addEventListener('click', function(){
-  inputField.value = '';
+  window.inputField.value = '';
 }, false);
 
-// Adding 'ce-button' click listener which removed last entered number
+// Adding 'ce-button' click listener which removed last entered number or operator
 ceButton.addEventListener('click', function(){
-  var inputFieldValue = inputField.value;
-  var lastIndex = inputFieldValue.length - 1;
-  var lastChar = inputFieldValue[lastIndex];
-  var inputArray = inputFieldValue.split(/(\x|-|\+|\/)/);
-  // returns an array where if last char was an operator, it has two more values in array
-  console.log(inputArray);
-  // TODO: If lastChar is x, -, + or /, remove two last indexes of array, otherwise remove 1.
-  var newArray = lastChar.match(/x|-|\+/) ? inputArray.slice(0, lastIndex-1) : inputArray.slice(0, lastIndex-2);
-  console.log(newArray);
+  var lastChar = window.inputField.value[window.inputField.value.length - 1];
+  var inputArray = inputField.value.split(/(\x|-|\+|\/)/);
+  var newValue = lastChar.match(/x|-|\+|\//) ? inputArray.slice(0, -2).join('') : inputArray.slice(0, -1).join('');
+  window.inputField.value = newValue;
 }, false);
 
-// TODO: 1. Create functions for AC/CE buttons.
-
-// Resource links:
-// http://demos.creative-tim.com/material-kit/components-documentation.html
-// https://www.sitepoint.com/dom-manipulation-vanilla-javascript-no-jquery/
+// TODO: Add function for equal button
