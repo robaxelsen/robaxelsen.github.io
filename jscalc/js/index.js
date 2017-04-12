97d89e39afe1c4357e3d60a13d1a1f319d2a0d07
robeerob <rob@rob.ee>
2017-04-12 14:47:38 +0200

    Work on "ce" and "ac" button functions, and fix missing dependencies

--- jscalc/js/index.js
+++ jscalc/js/index.js
@@ -4,30 +4,43 @@ var acButton = document.getElementById('ac-button');
 var ceButton = document.getElementById('ce-button');
 var inputField = document.getElementById('input-field');
 
-// TODO: 1. Create a function for all num buttons, grabbing value of clicked on DOM element.
+// Adding click listener to all number buttons, which grabs the value of the clicked button, and adds it to the input field.
 for (var i = 0; i < numButtons.length; i++) {
   numButtons[i].addEventListener('click', function(){
     inputField.value += this.value;
   }, false);
 };
 
-// TODO: 2. Create a function for all operator buttons and dot, except for equal.
+// Adding click listeners to all operator buttons (except equal sign) and period sign. 
 for (var i = 0; i < operatorButtons.length; i++) {
   operatorButtons[i].addEventListener('click', function(){
     inputField.value += this.value;
   }, false);
 };
 
-// TODO: 3. Create a function for equal button. (for now they both reset)
+// Adding 'ac-button' click listener which resets whole input field
 acButton.addEventListener('click', function(){
   inputField.value = '';
 }, false);
 
+// Adding 'ce-button' click listener which removed last entered number
 ceButton.addEventListener('click', function(){
-  inputField.value = '';
+  var inputFieldValue = inputField.value;
+  var inputFieldLastIndex = inputFieldValue.length - 1;
+  var lastInputChar = inputFieldValue[inputFieldLastIndex];
+  // TODO: Can not return +|/|x as lastInputChar. Investigate and fix
+  var inputArray = lastInputChar == ('x'|'-'|'+') ? 'it is' : 'it is not';
+  //var inputArray = inputFieldValue.split(/\x|\-|\+/);
+  if (Number(lastInputChar)) {
+    console.log(lastInputChar);
+    console.log(inputArray);
+    //inputField.value = inputField.value.substring(0, inputFieldLastIndex);
+  } else {
+    //inputField.value = inputField.value.substring(0, inputFieldLastIndex-2);
+  }
 }, false);
 
-// TODO: 4. Create functions for AC/CE buttons.
+// TODO: 1. Create functions for AC/CE buttons.
 
 // Resource links:
 // http://demos.creative-tim.com/material-kit/components-documentation.html
