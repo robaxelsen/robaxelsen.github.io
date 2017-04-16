f1710c7dc57fc5d64f177490408bb1b036e566b1
robeerob <rob@rob.ee>
2017-04-16 17:25:50 +0200

    Create initial html scaffolding, and countdown javascript

--- pomodoro/js/index.js
+++ pomodoro/js/index.js
@@ -1,1 +1,20 @@
-console.log('pomodoro');
+window.pomodoroCountdownId;
+window.pomodoroTime = 60;
+window.pomodoroButtonPressed = false;
+window.pomodoroTimeLeft = document.getElementById('timeleft');
+window.pomodoroGoButton = document.getElementById('gobutton');
+window.pomodoroGoButton.addEventListener('click', function() {
+  window.pomodoroCountdown();
+})
+window.pomodoroShowNextSecond = function() {
+  if (window.pomodoroTime > 0) {
+    window.pomodoroTime --;
+    window.pomodoroTimeLeft.innerHTML = window.pomodoroTime;
+  }
+}
+window.pomodoroCountdown = function() {
+  if (window.pomodoroButtonPressed === false) {
+    window.pomodoroButtonPressed = true;
+    window.pomodoroCountdownId = setInterval(window.pomodoroShowNextSecond, 1000);
+  }
+}
