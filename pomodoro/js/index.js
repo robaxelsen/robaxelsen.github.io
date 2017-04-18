ca58ef192aa58a33b77833b867de9059d03073eb
robeerob <rob@rob.ee>
2017-04-18 21:55:42 +0200

    Add TODOs, and implement else if for alert/alarm to go off at 0 seconds left

--- pomodoro/js/index.js
+++ pomodoro/js/index.js
@@ -1,15 +1,20 @@
 window.pomodoroCountdownId;
-window.pomodoroTime = 60;
+window.pomodoroTime = 10;
 window.pomodoroButtonPressed = false;
 window.pomodoroTimeLeft = document.getElementById('timeleft');
 window.pomodoroGoButton = document.getElementById('gobutton');
 window.pomodoroGoButton.addEventListener('click', function() {
   window.pomodoroCountdown();
 })
+// TODO: Change to minutes (instead of seconds), and implement
+// a dropdown to choose how many minutes before pressing 'Go!'
 window.pomodoroShowNextSecond = function() {
   if (window.pomodoroTime > 0) {
     window.pomodoroTime --;
     window.pomodoroTimeLeft.innerHTML = window.pomodoroTime;
+  } else if (window.pomodoroButtonPressed && window.pomodoroTime === 0) {
+    window.pomodoroButtonPressed = false;
+    alert('time is up');
   }
 }
 window.pomodoroCountdown = function() {
@@ -18,3 +23,7 @@ window.pomodoroCountdown = function() {
     window.pomodoroCountdownId = setInterval(window.pomodoroShowNextSecond, 1000);
   }
 }
+// TODO: Make a sound play when time is up, and make sure it
+// goes off also when in other tab
+
+// TODO: Check the other user stories, and plan them out here
