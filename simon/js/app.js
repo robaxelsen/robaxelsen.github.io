50b200405a748ce06f66a56334fd21040efc9e86
Robert Axelsen <rob@rob.ee>
2018-01-14 20:30:41 +0100

    Add initial styling and html

---
+++ simon/js/app.js
@@ -0,0 +1,9 @@
+console.log('js loaded');
+var simon = {
+	state: [
+		document.getElementById('cell1'),
+		document.getElementById('cell2'),
+		document.getElementById('cell3'),
+		document.getElementById('cell4')
+	]
+};
